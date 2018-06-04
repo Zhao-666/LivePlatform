7b03cabb3faf3835055d09eba7a51c5d699d48bd
Zhao-666 <2268699370@qq.com>
2018-06-04 08:08:55 +0800

    增加图片数据的上传

---
+++ application/admin/controller/Live.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2018/6/4
+ * Time: 8:06
+ */
+
+namespace app\admin\controller;
+
+
+class Live
+{
+    public function push()
+    {
+        print_r($_GET);
+    }
+}
