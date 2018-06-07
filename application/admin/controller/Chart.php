4bd0a7f2c1f3b414bb59f7c0c33229474faecf39
Zhao-666 <2268699370@qq.com>
2018-06-07 08:00:59 +0800

    新增聊天室代码

---
+++ application/admin/controller/Chart.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2018/6/7
+ * Time: 8:00
+ */
+
+namespace app\admin\controller;
+
+
+class Chart
+{
+    public function index()
+    {
+        
+    }
+}
--- public/static/live/js/chart-push.js
+++ public/static/live/js/chart-push.js
@@ -1,9 +1,9 @@
 $(function () {
     $('#discuss-box').keydown(function (event) {
-        if(event.keyCode == 13) {
+        if (event.keyCode == 13) {
             var text = $(this).val();
-            var url = "http://127.0.0.1:8811/?s=index/Chart/index";
-            var data = {'content':text, 'game_id':1};
+            var url = "http://172.20.10.4:8811/?s=index/Chart/index";
+            var data = {'content': text, 'game_id': 1};
 
             $.post(url, data, function (result) {
                 // todo
